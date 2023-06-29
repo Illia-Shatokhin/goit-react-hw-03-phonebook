778bd873a297415512877fdc4f9909b32e000827
Illia Shatokhin <resonane.2106@gmail.com>
2023-06-29 17:53:14 +0300

    add example template phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,14 @@ export class App extends Component {
   loadFromLocalStorage = key => {
     try {
       const serializedState = localStorage.getItem(key);
-      return serializedState === null ? [] : JSON.parse(serializedState);
+      return serializedState === null
+        ? [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+          ]
+        : JSON.parse(serializedState);
     } catch (error) {
       console.error('Get state error: ', error.message);
     }
